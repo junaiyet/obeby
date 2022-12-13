3b47547c880742fdb5ffecfdd1c91f009d4508fc
junaiyet <junaiyetmia470@gmail.com>
2022-12-13 13:54:44 +0600

    fddfgdfr

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import Image from "./component/layout/Image";
 import Flex from "./component/layout/Flex";
 import List from "./component/layout/List";
 import ListItem from "./component/layout/ListItem";
+import { RiBarChartHorizontalFill } from 'react-icons/ri';
+import { useEffect, useState } from "react";
 function App() {
+  let [show,setShow] = useState(false);
+  
+  useEffect(()=>{
+       function scrollWidth(e) {
+        console.log(e);
+       }
+       window.addEventListener("resize", scrollWidth);
+  },[])
+  
   return (
   <nav>
    <div className="max-w-container  mx-auto p-2.5">
@@ -10,7 +21,12 @@ function App() {
       <div className="logo w-1/4	">
         <Image imgsrc="assets/logo.png"/>
       </div>
+
       <div className="w-3/4	">
+    <RiBarChartHorizontalFill className="sm:hidden block ml-auto cursor-pointer" />
+      
+      {
+        show && 
        <List className="flex justify-end gap-x-10 ">
         <ListItem className="font-dm text-sm font-bold hover:font-bold cursor-pointer"  itemname="Home"/>
         <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="Shop"/>
@@ -18,6 +34,7 @@ function App() {
         <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="Contact"/>
         <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="Journal"/>
        </List>
+      }
 
       </div>
     </Flex>
