d08cc8dc78d1080c45ad7a42b0c89fbed73cb553
junaiyet <junaiyetmia470@gmail.com>
2022-12-16 19:46:43 +0600

    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import Image from "./component/layout/Image";
-import Flex from "./component/layout/Flex";
-import List from "./component/layout/List";
-import ListItem from "./component/layout/ListItem";
-import { RiBarChartHorizontalFill } from 'react-icons/ri';
+
 import { useEffect, useState } from "react";
+import Navber from "./component/layout/Navber";
+import { createBrowserRouter,RouterProvider,Route, Link,createRoutesFromElements } from "react-router-dom";
 function App() {
-  let [show,setShow] = useState(false);
-  
-  useEffect(()=>{
-       function scrollWidth(e) {
-        console.log(e);
-       }
-       window.addEventListener("resize", scrollWidth);
-  },[])
+let route =   createRoutesFromElements(
+  <Route path="/" element={<Navber/>}>
+  </Route>
+)
   
   return (
-  <nav>
-   <div className="max-w-container  mx-auto p-2.5">
-    <Flex className="flex">
-      <div className="logo w-1/4	">
-        <Image imgsrc="assets/logo.png"/>
-      </div>
-
-      <div className="w-3/4	">
-    <RiBarChartHorizontalFill className="sm:hidden block ml-auto cursor-pointer" />
-      
-      {
-        show && 
-       <List className="flex justify-end gap-x-10 ">
-        <ListItem className="font-dm text-sm font-bold hover:font-bold cursor-pointer"  itemname="Home"/>
-        <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="Shop"/>
-        <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="About"/>
-        <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="Contact"/>
-        <ListItem className="font-dm text-sm font-normal hover:font-bold cursor-pointer" itemname="Journal"/>
-       </List>
-      }
-
-      </div>
-    </Flex>
-   </div>
-  </nav>
+     <Navber/>
   );
 }
 
